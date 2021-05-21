91200a85bef2ad28ea325c16d576bddec7605af9
K_Pozoga <kpozoga99@gmail.com>
2021-05-21 14:35:28 +0200

    few more features

--- DigitRecognition/data_preparation/data_extraction.py
+++ DigitRecognition/data_preparation/data_extraction.py
@@ -6,7 +6,7 @@ from data_preparation.projection_vertical import projection_vertical
 from data_preparation.projection_horizontal import projection_horizontal
 from data_preparation.transition_horizontal import transition_horizontal
 from data_preparation.transition_vertical import transition_vertical
-
+import mahotas
 
 def extract_data(entity):
     projections = [projection_horizontal, projection_vertical, transition_horizontal, transition_vertical]
@@ -16,4 +16,8 @@ def extract_data(entity):
         values.extend([find_average(histogram), find_median(histogram)]
                       + list(find_max_position(histogram))
                       + list(find_min_position(histogram)))
+
+    values.append(mahotas.features.eccentricity(entity.pixels))
+    values.extend(list(mahotas.features.ellipse_axes(entity.pixels)))
+    values.append(mahotas.features.roundness(entity.pixels))
     return values
